286efadd306d01471dc506d65c4e9bc6560c8b4e
Lester Young <lyoung070789@gmail.com>
2020-02-26 19:40:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -8,6 +9,7 @@ class Scraper
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc = Nokogiri::HTML(html)
+    binding.pry
   end 
   
   def get_courses
@@ -31,5 +33,6 @@ class Scraper
   
 end
 
+Scraper.new.get_page
 
 
