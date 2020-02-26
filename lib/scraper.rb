0eb4d468cb62674abee6da973108e8e7e21377a0
Lester Young <lyoung070789@gmail.com>
2020-02-26 19:54:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,11 @@ class Scraper
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc = Nokogiri::HTML(html)
+    
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".")
     binding.pry
   end 
   
